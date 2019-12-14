0a1dbf5e52ed56097634141a3fb3f5a71f8ee2be
aniballajoat <51682015+aniballajoat@users.noreply.github.com>
2019-12-14 12:00:41 -0500

    Update login.py

--- interfaz/login.py
+++ interfaz/login.py
@@ -7,6 +7,10 @@
 # WARNING! All changes made in this file will be lost!
 
 
+
+
+#                                                                               Una miniguia de lo que es cada objeto esta retranslateUi
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox, QLabel, QApplication, QDialog
 
@@ -20,18 +24,23 @@ class Ui_MainWindow(object):
         self.window = QtWidgets.QMainWindow()
         self.ui = Ui_RegWindow()
         self.ui.setupUi(self.window)
-        #MainWindow.hide()
         self.window.show()
         
     def openRecupera(self):
         self.window = QtWidgets.QMainWindow()
         self.ui = Ui_RecuperaWindow()
         self.ui.setupUi(self.window)
-        #MainWindow.hide()
         self.window.show()
+    """                                                                         Para entrar a una nueva aplicacion
+    def entra_app(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = Ui_"nombre de la ventana nueva"
+        self.ui.setupUi(self.window)
+        self.window.show()
+    """
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 600)
+        MainWindow.resize(400, 600)                                                         #Mantengan este tamaño para las ventanas
         MainWindow.setAutoFillBackground(False)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -41,22 +50,25 @@ class Ui_MainWindow(object):
         
         self.Boton1.clicked.connect(self.show_popup)
         
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(100, 100, 211, 31))
-        self.textEdit.setObjectName("textEdit")
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_2.setGeometry(QtCore.QRect(100, 160, 211, 31))
-        self.textEdit_2.setObjectName("textEdit_2")
+        self.lineEdit_0 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_0.setGeometry(QtCore.QRect(100, 100, 211, 31))
+        self.lineEdit_0.setObjectName("lineEdit_0")
+        
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(100, 160, 211, 31))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setEchoMode(QtWidgets.QLineEdit.Password)
+        
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(100, 80, 61, 16))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(100, 140, 71, 16))
         self.label_2.setObjectName("label_2")
-		
-		
+        
+        
         self.label_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(100, 200, 131, 16))
+        self.label_3.setGeometry(QtCore.QRect(100, 200, 131, 25))
         self.label_3.setObjectName("label_3")
         
         self.label_3.clicked.connect(self.openRecupera)
@@ -66,6 +78,28 @@ class Ui_MainWindow(object):
         self.Boton1_2.setObjectName("Boton1_2")
         
         self.Boton1_2.clicked.connect(self.openReg)
+        
+        #                                                                                   Background//usen esto para todas las ventanas adicionales
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(0, 0, 456, 611))
+        self.label_4.setAutoFillBackground(False)
+        self.label_4.setText("")
+        self.label_4.setPixmap(QtGui.QPixmap("ca3rs.jpg"))
+        self.label_4.setScaledContents(True)
+        self.label_4.setObjectName("label_4")
+        self.label_4.raise_()
+        
+        
+        
+        
+        #                                                                                   Trae los demas objetos al frente
+        self.Boton1.raise_()
+        self.lineEdit_0.raise_()
+        self.lineEdit.raise_()
+        self.label.raise_()
+        self.label_2.raise_()
+        self.label_3.raise_()
+        self.Boton1_2.raise_()
         
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -79,13 +113,31 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         
+    #                                                                                       Se activa para probar datos
     def show_popup(self):
+        
         msg = QMessageBox()
-        msg.setWindowTitle("Bienvenido!")
-        msg.setText("Bienvenido al programa!")
+        usuario=self.lineEdit_0.text()
+        password=self.lineEdit.text()
+        if (usuario=="" or password==""):
+            msg.setWindowTitle("Error")
+            msg.setText("No puede dejar espacios en blanco!")
+            
+            #                                                                           Aqui revisen datos con usuario y password
+            """
+            #en caso de error
+            elif (len(password)<6):
+                msg.setWindowTitle("Error")
+                msg.setText("No se puede dejar en blanco!")"""
+            
+        else:
+            
+            msg.setWindowTitle("Bienvenido!")
+            msg.setText("Bienvenido al programa!")
+            #                                                                           llamen para acceder a la ventana
         
         x = msg.exec_()
-
+#                                                                                       Aqui revisen cara herramienta con su nombre
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
